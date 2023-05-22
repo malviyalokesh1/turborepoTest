1359ea9aaacf927f6314338d4c22486c38b6502d
lokesh <lokesh.malviya@edgistify.com>
2023-05-22 12:29:31 +0530

    storybooks configured

--- apps/workshop/.storybook/main.ts
+++ apps/workshop/.storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-interactions'
   ],
   framework: {
     name: '@storybook/react-vite',
