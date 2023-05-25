6295c8ebcb1abb6fb1a8af91329353497641dd52
lokesh <lokesh.malviya@edgistify.com>
2023-05-25 09:39:37 +0530

    init dev branch

--- packages/ui/atoms/button/button.stories.ts
+++ packages/ui/atoms/button/button.stories.ts
@@ -6,11 +6,11 @@ import { Button } from './button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: 'Example/Button',
+  title: 'Components/Atoms/Button',
   component: Button,
   tags: ['autodocs'],
   argTypes: {
-    variant: { control: 'variant' },
+    clickHandler: { action: 'buttonClick' },
   },
 } satisfies Meta<typeof Button>;
 
